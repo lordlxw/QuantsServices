1685103d83312da1dec0dee2a8e4cc997d40ac9f
刘鑫炜 <10749320+liu-xinwei980714@user.noreply.gitee.com>
2025-01-13 16:57:51 +0800

    添加通用response类

---
+++ QuantsWebApiService/Models/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace QuantsWebApiService.Models
+{
+    public class ApiResponse
+    {
+        public string Msg { get; set; }
+        public int Code { get; set; }
+        public string Token { get; set; }
+    }
+
+}
